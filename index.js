ce825afb0a051064d98a662589741a2f9e3dbfaf
Jisu70 <126372181+Jisu70@users.noreply.github.com>
2023-06-21 06:13:34 +0000

    completed jwt authentication

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-const express = require('express');
+// Dependencies
+const express = require("express");
 
 const app = express();
 
-const router = require('./routhandler/main.route');
+const router = require("./routhandler/main.route");
 
-const database = require('./util/db.connection.js');
+// Requring datbase connection
+const database = require("./util/db.connection.js");
 
+// To parse the json data
+app.use(express.json());
+
+// Call the dtabase to connect
 database();
 
 app.use(router);
 
 app.listen(3000, () => {
-  console.log(' app listen on port 3000');
+  console.log(" app listen on port 3000");
 });
